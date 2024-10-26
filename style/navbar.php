7e3357552ff2a16c6cfdea6e45a690365a9ea7c2
Gwyn <skythekid211@gmail.com>
2024-10-26 22:07:44 +0800

    sticky navbar

--- style/navbar.php
+++ style/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffccbc;">
+<nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #ffccbc;">
     <div class="container">
         <a class="navbar-brand text-primary" href="#">Math Barn</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
